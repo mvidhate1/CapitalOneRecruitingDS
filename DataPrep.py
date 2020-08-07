536dee0d88e4d1d09d2169a1e6d615070c04fb58
Maneesh Vidhate <maneeshvidhate@ManMac.local>
2020-08-07 15:38:50 +0400

    Analysis & Sampling

--- DataPrep.py
+++ DataPrep.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 
 def get_data(url):
+
     r = requests.get(url)
     z = zipfile.ZipFile(io.BytesIO(r.content))
     z.extractall()
@@ -14,18 +15,60 @@ def get_data(url):
     base = path.splitext(file_name)[0]
     os.rename(file_name, base + ".json")
 
-    new_file_name = base+".json"
+    new_file_name = base + ".json"
 
     return new_file_name
 
 def get_databuffer(fname):
+
     lines = []
+
     with open(fname, "r") as fp:
         for line in fp:
             obj = json.loads(line)
             lines.append(obj)
 
-    data = pd.DataFrame(lines)
+    # serialize and deserialize??
+    df = pd.DataFrame(lines)
+    df = df.replace(r'^\s*$', 'NA', regex=True)
 
-    data = data.replace(r'^\s*$', np.nan, regex=True)
-    print(data.loc[1])
+    return df
+
+def data_prep(df):
+
+    df.insert(6, 'overDraw', 0)
+    df.insert(8, 'storeID', 'NA')
+    df.insert(11, 'overSeas', 0)
+    df.insert(18, 'addressChangeActivity', 0)
+    df.insert(21, 'cvvMatch', 0)
+
+    for i in range(0, len(df)):
+        item = df.loc[i]
+
+        merchantName = item['merchantName']
+        merchantName = merchantName.split(' #')
+        if len(merchantName) == 2:
+            df.at[i, 'storeID'] = merchantName[1]
+        df.at[i, 'merchantName'] = str(merchantName[0])
+
+        availMoney = item['availableMoney']
+        transAmt = item['transactionAmount']
+        if availMoney - transAmt <= 0:
+            df.at[i, 'overDraw'] = 1
+
+        acqCountry = item['acqCountry']
+        merchCountry = item['merchantCountryCode']
+        if acqCountry != merchCountry:
+            df.at[i, 'overSeas'] = 1
+
+        dateOpen = item['accountOpenDate']
+        dateAddrChange = item['dateOfLastAddressChange']
+        if dateOpen != dateAddrChange:
+            df.at[i, 'addressChangeActivity'] = 1
+
+        cvvEntered = item['enteredCVV']
+        cvvCard = item['cardCVV']
+        if cvvEntered == cvvCard:
+            df.at[i, 'cvvMatch'] = 1
+
+    return df
--- main.py
+++ main.py
@@ -1,10 +1,48 @@
-import sys
-import DataPrep
+import pandas as pd
+import DataPrep as DP
+import DataAnalysis as DA
+import DataSampling as DS
 
 def main():
-    fname = DataPrep.get_data("https://raw.githubusercontent.com/CapitalOneRecruiting/DS/master/transactions.zip")
-    DataPrep.get_databuffer(fname)
+    # GET DATASET AND STORE AS JSON FILE
+    # fname = DP.get_data("https://raw.githubusercontent.com/CapitalOneRecruiting/DS/master/transactions.zip")
 
+    # CONVERT TO DATAFRAME AND FILL IN BLANKS
+    # df = DP.get_databuffer(fname)
+
+    # SAVE TO PICKLE FOR EASY AND FAST ACCESS
+    # df.to_pickle('data.pkl')
+    df = pd.read_pickle('data1.pkl')
+    print(df.loc[0])
+    # df = DP.data_prep(df)
+    # df.to_pickle('data1.pkl')
+    # print(df.loc[0])
+
+    # ANALYSIS
+    # DA.get_isFraudTrue(df)
+    # DA.accNum_check(df)
+    # DA.overdraw_check(df)
+    # DA.creditLimit_check(df)
+    # DA.merchName_check(df)
+    # DA.merchCat_check(df)
+    # DA.location_check(df)
+    # DA.posEntryMode_check(df)
+    # DA.posCond_check(df)
+    # DA.dateAddressChange_check(df)
+    # DA.CVV_check(df)
+    # DA.transactionType_check(df)
+    # DA.card_check(df)
+    # DA.expDateMatched_check(df)
+
+    # SAMPLING
+    stratify_parameters = ['creditLimit', 'overDraw', 'overSeas', 'posEntryMode', 'posConditionCode', 'merchantCategoryCode', 'addressChangeActivity', 'cvvMatch', 'transactionType', 'cardPresent', 'expirationDateKeyInMatch', 'isFraud']
+    stratified_report = DS.stratified_sample_report(df, ['isFraud'])
+    print(stratified_report)
+    # with pd.ExcelWriter('sampleCount.xlsx') as writer:
+    #     stratified_report.to_excel(writer)
+
+    stratified_sample = DS.stratified_sample(df, ['isFraud'])
+    print(stratified_sample)
 
 if __name__ == "__main__":
     main()
